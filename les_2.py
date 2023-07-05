59a0a9b692c0fc21285f7dd6891ad8064d04378d
suliwxanpythondev <suliwxanindiaminova@gmail.com>
2023-07-05 16:20:39 +0300

    code

---
+++ les_2.py
@@ -0,0 +1,11 @@
+#1.esap
+
+#2.esap
+a =int(input('san kiritin : '))
+b =int(input('san kiritin : '))
+for i in range(a,b+1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+
+#3.esap
---
+++ les_3.py
@@ -0,0 +1,124 @@
+'''def func():
+    print('hello world')
+func()
+
+def func():
+    print('Indiaminova Suliwxan')
+func()
+
+def func():
+    a = int(input('san: '))
+    b = int(input('san: '))
+    print(a+b)
+func()
+
+def func():
+    a = int(input('san: '))
+    b = int(input('san: '))
+    print(a*b)
+func()
+def func(): 
+    a = int(input('san: '))
+    b = int(input('san: '))
+    print(a/b)
+func()
+
+def func(): 
+    a = int(input('san: '))
+    b = int(input('san: '))
+    print(a-b)
+func()
+
+def func(): 
+    a = int(input('san: '))
+    b = int(input('san: '))
+    print(a**b)
+func()
+
+def func(): 
+    a = int(input('san: '))
+    b = int(input('san: '))
+    print(a%b)
+func()
+
+def func(): 
+    a = int(input('san: '))
+    b = int(input('san: '))
+    print(a//b)
+func()
+
+def func(name): #argumentny funcsiya
+    print(name)
+func('Suliwxan')
+
+def func(lastname)
+    print(lastname)
+func('Indiaminova')
+
+def func(a,b):
+    print(a+b)
+a = int(input('san_1 : '))
+b = int(input('san_2 : '))
+func(a,b)
+
+def qosiw(a,b):
+    print(a+b)
+qosiw(2,4)
+
+def kob(a,b):
+    print(a*b)
+kob(2,4)
+
+def aliw(a,b):
+    print(a-b)
+aliw(2,4)
+
+def boliw(a,b):
+    print(a/b)
+boliw(2,4)
+
+def qosiw(a,b):
+    print(a%b)
+qosiw(2,4)
+
+def qosiw(a,b):
+    print(a//b)
+qosiw(2,4)
+
+def func():
+    sl = int(input('jas: '))
+    if sl > 10:
+        print('Oqiwga tapsira alasan!!!')
+    else:
+        print('Oqiwga tapsiralmaysan')
+func()
+
+def func():
+    sl = int(input('jasniz : '))
+    if sl == 22:
+        print(f'sizdin jiliniz: {2023-sl}')
+    else:
+        print('siz jasinizdi qayttan kiritn!!!')
+func()
+
+def func():
+    sl = int(input('jasniz : '))
+    for i in sl:
+        if sl == 22:
+            print(f'sizdin jiliniz: {2023-sl}')
+        else:
+            print('siz jasinizdi qayttan kiritn!!!')
+func()'''
+
+def func():
+    sl =[]
+    sl.append('b')
+    sl.append('a')
+    sl.append('c')
+    sl.append('d')
+    sl.append('g')
+    sl.append('k')
+    print(sl)
+    print(len(sl))
+    print(sorted(sl))
+func()
